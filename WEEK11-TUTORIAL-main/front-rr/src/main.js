a4992cdc06ea528547aeb19eb30acb26fc775430
whiskas69 <64070075@it.kmitl.ac.th>
2023-05-02 00:42:29 +0700

    add

---
+++ WEEK11-TUTORIAL-main/front-rr/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import 'bulma/css/bulma.css'
+
+
+Vue.config.productionTip = false
+
+
+
+new Vue({
+  router,  // add router here
+  render: h => h(App),
+}).$mount('#app')
